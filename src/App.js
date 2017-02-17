050e3ba438993ab00398509275e197da566c6973
Junior Oliveira <arojunior@gmail.com>
2017-02-17 10:51:05 -0200

    search form container created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import SearchForm           from './containers/searchForm'
 
 class App extends Component {
   render() {
     return (
-
+        <div className="container">
+            <SearchForm />
+        </div>
     )
   }
 }
--- src/containers/searchForm.js
+++ src/containers/searchForm.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
 
-class SearchForm extends Component{
+class SearchForm extends Component {
+
+    handleSubmit(e) {
+      e.preventDefault()
+    }
+
     render() {
         return (
-          <div className="container">
-          </div>
+            <div className="jumbotron">
+              <h1>GitHub Info</h1>
+              <div className="row">
+                <form onSubmit={() => this.handleSubmit}>
+                  <div className="form-group">
+                    <label>Username</label>
+                    <input
+                        type="text"
+                        ref="username"
+                        className="form-control"
+                        placeholder="Ex: arojunior"
+                      />
+                  </div>
+                  <button type="submit" className="btn btn-primary">Search
+                  </button>
+                </form>
+              </div>
+            </div>
         )
   }
 }
